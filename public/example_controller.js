cdbb4ce3f6b794bb1d239d90c0fba39533616d18
Hayden Faulds <fauldsh@gmail.com>
2014-08-02 01:05:55 +0100

    change vals shown

--- public/example_controller.js
+++ public/example_controller.js
@@ -1,7 +1,14 @@
 function ExampleController($scope) {
-  $scope.headings = 'abcdefghijklmnopqrstuvwxyz'.split('');
-  $scope.rows = _(51).times(function(n) { return n });
-  $scope.columns = _(51).times(function(n) { return n });
+  $scope.column_names = _.flatten(_(2).times(function() {
+    return _.union('abcdefghijklmnopqrstuvwxyz'.split(''))
+  }));
+  $scope.column_names[3] = 'long heading';
+
+  $scope.row_names = 'abcdefghijklmnopqrstuvwxyz'.split('');
+
+  $scope.column_vals = _(51).times(function(n) { return n });
+  $scope.column_vals[6] = 'long column';
+
   $(function() {
     $('table').sticky_table();
   });
