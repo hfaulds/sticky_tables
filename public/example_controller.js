8534c098c1d9813dce9daaf4b6f5d140a68b7ee6
Hayden Faulds <fauldsh@gmail.com>
2014-08-02 15:29:45 +0100

    jshint errors

--- public/example_controller.js
+++ public/example_controller.js
@@ -1,12 +1,12 @@
 function ExampleController($scope) {
   $scope.column_names = _.flatten(_(2).times(function() {
-    return _.union('abcdefghijklmnopqrstuvwxyz'.split(''))
+    return _.union('abcdefghijklmnopqrstuvwxyz'.split(''));
   }));
   $scope.column_names[3] = 'long heading';
 
   $scope.row_names = 'abcdefghijklmnopqrstuvwxyz'.split('');
 
-  $scope.column_vals = _(51).times(function(n) { return n });
+  $scope.column_vals = _(51).times(function(n) { return n; });
   $scope.column_vals[6] = 'long column';
 
   $(function() {
