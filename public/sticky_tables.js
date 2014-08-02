eaf4c78ecc92533fe7140fd9f9e205d44e093c10
Hayden Faulds <fauldsh@gmail.com>
2014-08-02 01:13:23 +0100

    minor refactoring

--- public/sticky_tables.js
+++ public/sticky_tables.js
@@ -1,13 +1,17 @@
 (function($) {
   function StickyTable(table, settings) {
     var default_settings = {
-      inner_class: 'stick-table-fix-width'
+      inner_class: 'stick-table-fix-width',
+      sticky_column_headings: true
     };
     this.table = table;
     this.thead = table.find('thead');
     this.tbody = table.find('tbody');
 
     this.settings = _.extend(default_settings, settings);
+    if (this.settings.sticky_column_headings) {
+      this.setupColumnHeadings();
+    }
   }
 
   StickyTable.prototype.add_wrapper = function(div) {
@@ -15,7 +19,7 @@
     return div.children();
   };
 
-  StickyTable.prototype.init = function() {
+  StickyTable.prototype.setupColumnHeadings = function() {
     var self = this;
     self.thead.children().each(function() {
       var thead_row = $(this);
@@ -32,36 +36,44 @@
       });
     });
 
-    this.thead_clone = this.thead.clone();
-    this.thead_clone.hide();
-    this.thead.before(this.thead_clone);
-    this.thead_clone.css({
+    this.column_headings = this.thead.clone();
+    this.column_headings.hide();
+    this.column_headings.css({
       'top': 0,
       'left': this.thead.position().left,
       'position': 'fixed',
       'z-index': 5
     });
-    this.scroll();
+    this.thead.before(this.column_headings);
+
+    this.scrollColumnHeadings();
   };
 
   StickyTable.prototype.scroll = function() {
+    if(this.settings.sticky_column_headings) {
+      this.scrollColumnHeadings();
+    }
+    //handle sticky row names
+  };
+
+  StickyTable.prototype.scrollColumnHeadings = function() {
     if($(window).scrollTop() > this.table.offset().top) {
+      // handle scrolling past
       var maxScroll = $(document).width() - $(window).width();
       var scrollLeft = Math.min(Math.max(0, $(window).scrollLeft()), maxScroll);
 
-      this.thead_clone.show();
-      this.thead_clone.css({
+
+      this.column_headings.show();
+      this.column_headings.css({
         'left': this.table.offset().left-scrollLeft,
       });
     } else {
-      this.thead_clone.hide();
+      this.column_headings.hide();
     }
   };
 
-
   $.fn.sticky_table = function(settings) {
     var table = new StickyTable(this, settings)
-    table.init();
     $(window).scroll($.proxy(table.scroll, table));
   };
 })(jQuery);
